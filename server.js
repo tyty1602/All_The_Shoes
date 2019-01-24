68b857f4a153c616e9821bd9ba5b3803778599f5
tyty1602 <37279775+tyty1602@users.noreply.github.com>
2019-01-23 20:13:12 -0800

    files

--- server.js
+++ server.js
@@ -24,8 +24,15 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './app_client/build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './app_client/', './public/index.html'));
+  res.sendFile(path.join(__dirname, './app_client/build', 'index.html'));
 });
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// -app.get('/', function (req, res) {
+// +app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 
 
 app.get('/scrape', function (req, res) {
